1efda8ba13be5882b40f4d3d3eb99115a623c909
jhiggi44 <jordandhiggins@gmail.com>
2020-11-10 17:56:20 -0700

    Backfill tests for word class

--- word.test.js
+++ word.test.js
@@ -1,28 +1,47 @@
 const { expect } = require('@jest/globals');
 const { Word } = require('./word');
-const characterFor = require('./character').characterFor;
+const { Character } = require('./character');
 
-test('word is not complete', () => {
-    const word = new Word("expelliarmus");
-    expect(word.isComplete()).toBeFalsy;
-});
-
-test('word is complete', () => {
-    const word = new Word("expelliarmus");
-    word.letters.forEach(letter => {
-        letter.show = true;
-    });
-    expect(word.isComplete()).toBeTruthy;
-});
-
-test('matching characters', () => {
-    const word = new Word("expelliarmus");
-    const guess = characterFor("l");
-    const matchingCharacters = word.charactersMatching(guess);
-    matchingCharacters.forEach(char => {
-        char.setDisplayToValue();
+describe('isComplete', () => {
+    test('when the word is not complete', () => {
+        const word = new Word("expelliarmus");
+        expect(word.isComplete()).toBeFalsy;
     });
 
-    expect(matchingCharacters.length).toEqual(2);
-    expect(word.toString()).toBe("_ _ _ _ l l _ _ _ _ _ _")
+    test('when the word is complete', () => {
+        const word = new Word("expelliarmus");
+        word.letters.forEach(letter => {
+            letter.show = true;
+        });
+        expect(word.isComplete()).toBeTruthy;
+    });
+});
+
+describe('charactersMatching', () => {
+    test('when the character has matches', ()=> {
+        const word = new Word("expelliarmus");
+        const guess = Character.for("e");
+        const matchingCharacters = word.charactersMatching(guess);
+        expect(matchingCharacters.length).toEqual(2);
+        expect(matchingCharacters).toContainEqual(guess);
+    });
+
+    test('when the character has no matches', ()=> {
+        const word = new Word("expelliarmus");
+        const guess = Character.for("f");
+        const matchingCharacters = word.charactersMatching(guess);
+        expect(matchingCharacters.length).toEqual(0);
+    });
+});
+
+describe('toString', () => {
+    test('shows only characters that are meant to be shown', ()=> {
+        const word = new Word("foo");
+        
+        word.charactersMatching(Character.for("o")).forEach((character) => {
+            character.setDisplayToValue();
+        });
+
+        expect(word.toString()).toBe("_ o o");
+    });
 });
